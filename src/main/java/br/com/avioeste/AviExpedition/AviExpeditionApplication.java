dd0be2b778caa0a777c13a4f5d2c6b112e59b29c
GiovanniDonati <grdonati99@gmail.com>
2025-07-08 21:24:09 -0300

    feat: Initialize the project struture with Java + SpringBoot

---
+++ src/main/java/br/com/avioeste/AviExpedition/AviExpeditionApplication.java
@@ -0,0 +1,13 @@
+package br.com.avioeste.AviExpedition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviExpeditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviExpeditionApplication.class, args);
+	}
+
+}
